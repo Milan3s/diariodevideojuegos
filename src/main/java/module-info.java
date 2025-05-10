2ccc06578361b069fd9c931ecaabd9be61ba52e9
Milanes <Milanes@Mila>
2025-05-10 13:22:16 +0200

    añadida la dependencia  Jfoenix

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,17 @@
 module main {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
     requires java.sql;
     requires java.base;
-    requires javafx.media;
-    requires de.jensd.fx.glyphs.fontawesome; // <- necesario para usar FontAwesomeIconView
+
+    requires com.jfoenix; // ✅ JFoenix (Material Design for JavaFX)
+    requires de.jensd.fx.glyphs.fontawesome; // FontAwesomeFX
 
     opens main to javafx.fxml, javafx.graphics;
     opens controllers to javafx.fxml;
+    opens models to javafx.base; // Si usas modelos en bindings o TableView
+    opens dao to javafx.base;    // Opcional: si accedes desde FXML
 
     exports main;
     exports controllers;
