5e93608213c40400e17a405f4eb23aaf8d6c0786
Milanes <Milanes@Mila>
2025-05-16 00:43:40 +0200

    ok

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -3,10 +3,8 @@ package main;
 import config.AppLogger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
